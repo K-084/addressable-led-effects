43bf2b217b4bdf57ffb17fabf6c7c89fd5005449
K-084 <50931322+K-084@users.noreply.github.com>
2021-03-31 11:24:26 -0700

    Added Chroma effects

--- RGBChroma/main.cpp
+++ RGBChroma/main.cpp
@@ -8,58 +8,36 @@
  */
 
 #include <Adafruit_NeoPixel.h>
+#ifdef __AVR__
+#include <avr/power.h>
+#endif
 
-#define PIN1 2
-#define PIN2 3
-const int numPixels1 = 240;
-const int numPixels2 = 240;
-Adafruit_NeoPixel strip1;
-Adafruit_NeoPixel strip2;
+#define PIN 0
+#define numLeds 0
 
-int countPixel = 0;
-int countVal = 0;
-byte r = 0;
-byte g = 0;
-byte b = 0;
-byte w = 0;
-int p = 0;
-bool isOn = false;
-byte command = 0;
-bool isCommand = false;
+// Parameter 1 = number of pixels in strip
+// Parameter 2 = Arduino pin number (most are valid)
+// Parameter 3 = pixel type flags, add together as needed:
+//   NEO_KHZ800  800 KHz bitstream (most NeoPixel products w/WS2812 LEDs)
+//   NEO_KHZ400  400 KHz (classic 'v1' (not v2) FLORA pixels, WS2811 drivers)
+//   NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
+//   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
+//   NEO_RGBW    Pixels are wired for RGBW bitstream (NeoPixel RGBW products)
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(numLeds, PIN, NEO_GRB + NEO_KHZ800);
 
-void setup()
-{
-  strip1 = Adafruit_NeoPixel(numPixels1, PIN1, NEO_GRBW + NEO_KHZ800);
-  delay(100);
-  strip2 = Adafruit_NeoPixel(numPixels2, PIN2, NEO_GRBW + NEO_KHZ800);
-  delay(100);
-  // start serial port and wait for port to open:
-  Serial.begin(115200);
-  Serial1.begin(115200);
-  strip1.setBrightness(255);
-  strip1.begin();
-  strip1.show(); // Initialize all pixels to 'off'
-  strip2.setBrightness(255);
-  strip2.begin();
-  strip2.show(); // Initialize all pixels to 'off'
+// IMPORTANT: To reduce NeoPixel burnout risk, add 1000 uF capacitor across
+// pixel power leads, add 300 - 500 Ohm resistor on first pixel's data input
+// and minimize distance between Arduino and first pixel.  Avoid connecting
+// on a live circuit...if you must, connect GND first.
+
+void setup() {
+  strip.begin();
+  strip.show(); // Initialize all pixels to 'off'
+
 }
 
-void loop()
-{ 
-  if (Serial1.available()>0)
-  {
-    int value = Serial1.read();
-    if(value == 255 && command == 0)
-    {
-      isCommand = true;
-    }
-    else if(isCommand && command == 0)
-    {
-      command = value;
-    }
-    else
-    {
-      handleCommand(command, value);
-    }
-  }
+void loop() {
+  rainbowCycle(20);
+
 }
+
