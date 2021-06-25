5ce3db4355ed57ff6c88a91312a5f0376a6ac0e5
uzrzero <50931322+uzrzero@users.noreply.github.com>
2021-06-24 23:55:24 -0400

    Update effects.cpp

--- src/PoliceLights/effects.cpp
+++ src/PoliceLights/effects.cpp
@@ -132,6 +132,10 @@ class effects {
       }
     }
 
+    /**
+     * Turn on ends of the light strips
+     * @param c
+     */
     void OnOuter(uint32_t c) {
       strip.setPixelColor(strip.numPixels() - 1, c);
       strip.setPixelColor(0, c);
